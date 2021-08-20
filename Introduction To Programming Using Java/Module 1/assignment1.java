6aca543fc1bd9ebbeec0d6ef7aef6c36da66b365
Soumil Verma <soumil_verma@yahoo.com>
2021-08-19 21:06:10 -0400

    Introduction To Programming Using Java

---
+++ Introduction To Programming Using Java/Module 1/assignment1.java
@@ -0,0 +1,9 @@
+public class assignment1 {
+
+	public static void main(String[] args) {
+		System.out.print("    J    A    V     V    A    \n");
+		System.out.print("    J   A A    V   V    A A   \n");
+		System.out.print("J   J  AAAAA    V V    AAAAA  \n");
+        System.out.print(" J J  A     A    V    A     A \n");
+	}
+}
